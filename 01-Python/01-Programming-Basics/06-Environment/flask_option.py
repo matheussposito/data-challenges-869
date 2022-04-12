f24733613dc20932ac80585dee108498013c04a6
Matheus Oliveira <matheus_so@yahoo.com.br>
2022-04-11 22:50:22 -0300

    environment variables

--- 01-Python/01-Programming-Basics/06-Environment/flask_option.py
+++ 01-Python/01-Programming-Basics/06-Environment/flask_option.py
@@ -4,7 +4,9 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    if os.environ.get('FLASK_ENV', 'Starting in production mode...') == 'development':
+        return 'Starting in development mode...'
+    return 'Starting in production mode...'
 
 if __name__ == "__main__":
     print(start())
