e605718e5cbdbef717aa9dcd93c42fe4b0029b44
Matheus Oliveira <matheus_so@yahoo.com.br>
2022-04-14 18:44:23 -0300

    rank and partition

--- 01-Python/04-SQL-Advanced/03-Rank-And-Partition/queries.py
+++ 01-Python/04-SQL-Advanced/03-Rank-And-Partition/queries.py
@@ -1,8 +1,28 @@
 # pylint:disable=C0111,C0103
 
 def order_rank_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = '''
+    SELECT o.OrderId, o.CustomerID, o.OrderDate,
+    RANK() OVER(
+    PARTITION BY o.CustomerID
+    ORDER BY o.OrderDate) AS OrderRank
+    FROM Orders o
+    '''
+    db.execute(query)
+    r = db.fetchall()
+    return r
 
 
 def order_cumulative_amount_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = '''
+    SELECT o.OrderId, o.CustomerID, o.OrderDate,
+    SUM(SUM(od.UnitPrice*od.Quantity)) OVER(
+    PARTITION BY o.CustomerID
+    ORDER BY o.OrderDate) AS OrderCumulativeAmount
+    FROM Orders o
+    JOIN OrderDetails od ON o.OrderID = od.OrderID
+    GROUP BY o.OrderID
+    '''
+    db.execute(query)
+    r = db.fetchall()
+    return r
