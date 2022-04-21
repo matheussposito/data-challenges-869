af55f6ba6a4077e9db7fa3a1be6feed016286653
Matheus Oliveira <matheus_so@yahoo.com.br>
2022-04-21 17:21:30 -0300

    data_prepation notebook and implemented the get_data function in data.py

--- 04-Decision-Science/olist/data.py
+++ 04-Decision-Science/olist/data.py
@@ -10,11 +10,26 @@ class Olist:
         Its values should be pandas.DataFrames loaded from csv files
         """
         # Hints 1: Build csv_path as "absolute path" in order to call this method from anywhere.
-            # Do not hardcode your path as it only works on your machine ('Users/username/code...')
-            # Use __file__ instead as an absolute path anchor independant of your usename
-            # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
+        # Do not hardcode your path as it only works on your machine ('Users/username/code...')
+        # Use __file__ instead as an absolute path anchor independant of your usename
+        # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
         # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
-        pass  # YOUR CODE HERE
+        path = os.path.dirname(os.path.dirname(__file__))
+        csv_path = os.path.join(path, 'data', 'csv')
+        file_names = [
+            file for file in os.listdir(csv_path) if file.endswith('csv')
+            ]
+
+        key_names = [
+            elem[:-4].replace('olist_', '').replace('_dataset', '')
+            for elem in file_names
+        ]
+
+        data = {}
+        for key, path in zip(key_names, file_names):
+            data[key] = pd.read_csv(os.path.join(csv_path, path))
+
+        return data
 
     def ping(self):
         """
