49b566511e98dbcae7501d63f3e4e1ce655c8f56
Ellyn <ellyn.bouscasse@gmail.com>
2022-05-04 09:17:24 +0200

    Update test_recipe.py
    fix conflict

--- 01-Python/02-Data-Sourcing/03-Scraping/tests/test_recipe.py
+++ 01-Python/02-Data-Sourcing/03-Scraping/tests/test_recipe.py
@@ -5,13 +5,8 @@ from recipe import parse
 class TestRecipe(unittest.TestCase):
     def test_parse_carrot(self):
         recipes = parse(open('pages/carrot.html'))
-<<<<<<< HEAD
-        self.assertIsInstance(recipes, list, "The `parse` method should return a `list`")
-        self.assertIsInstance(recipes[0], dict, "The `parse_recipe` method should return a `list` of `dict` objects")
-=======
         self.assertIsInstance(recipes, list, "The `parse` method should return a `list` of `dict` objects")
         self.assertIsInstance(recipes[0], dict, "The `parse_recipe` method should return a `dict` object")
->>>>>>> master
         self.assertEqual(recipes[0]['name'], 'Beef Carrot Stew')
         self.assertEqual(recipes[0]['difficulty'], 'Very easy')
         self.assertEqual(recipes[0]['prep_time'], '45 min')
